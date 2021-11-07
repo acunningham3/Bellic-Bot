fa48d1984c06483cc031e685a0a6c2b75cb084f4
Adam Cunningham <adamcunningham03@gmail.com>
2021-11-06 20:33:14 -0400

    Hide token better through env

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const discord = require('discord.js');  //Include discord libs
+require('dotenv').config();             //Init dotenv for TOKEN distribution
 
 const client = new discord.Client({     //Create bot instance and define intents
     intents: [
         discord.Intents.FLAGS.GUILDS,
         discord.Intents.FLAGS.GUILD_MESSAGES
 ]});    
-
 
 client.once('ready', () => {    //If client successfully starts up then give message
     console.log('Bellic is online!');
@@ -17,4 +17,4 @@ client.on('messageCreate', (message) => {   //Simple command to send pong messag
     }
 })
 
-client.login('OTA2NjgwMTM2MTkwMDA5MzU0.YYcJrQ.w2vj7WWDh06FJNEwcSndTNrUUCg');    //Bot secret token
+client.login(process.env.TOKEN);    //Login to bot
