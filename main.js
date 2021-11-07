94bb4e4e15e3c8ef5c2c9ac2b5354ccf34a8bc77
Adam Cunningham <adamcunningham03@gmail.com>
2021-11-06 20:15:21 -0400

    basic setup

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const discord = require('discord.js');  //Include discord libs
 
-const client = new discord.client();    //Create bot instance
+const client = new discord.Client({     //Create bot instance and define intents
+    intents: [
+        discord.Intents.FLAGS.GUILDS,
+        discord.Intents.FLAGS.GUILD_MESSAGES
+]});    
+
+
+client.once('ready', () => {    //If client successfully starts up then give message
+    console.log('Bellic is online!');
+});
+
+client.on('messageCreate', (message) => {   //Simple command to send pong message for ping
+    if (message.content === 'ping') {
+        message.reply({content: 'pong'})
+    }
+})
+
+client.login('OTA2NjgwMTM2MTkwMDA5MzU0.YYcJrQ.w2vj7WWDh06FJNEwcSndTNrUUCg');    //Bot secret token
